b2008aa5d4766268b3bc07d1d2186a427896095c
Mayline Perez <mperezg78@miumg.edu.gt>
2025-05-15 20:29:09 -0600

    se creo la tabla de clientes y se agrego al main

--- Proyecto Final/Proyecto Final/Proyecto Final.cpp
+++ Proyecto Final/Proyecto Final/Proyecto Final.cpp
@@ -1,6 +1,7 @@
 #include <mysql.h>
 #include <iostream>
 #include "Empleados.h"
+#include "Clientes.h"
 
 using namespace std;
 
@@ -87,11 +88,82 @@ void menuEmpleados() {
     } while (opcion != 5);
 }
 
+void menuClientes() {
+    Clientes c;
+    int opcion = 0;
+
+    do {
+        cout << "\n--- Menu Clientes ---\n";
+        cout << "1. Crear\n2. Leer\n3. Actualizar\n4. Borrar\n5. Salir\n";
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
+        cin.ignore();
+
+        switch (opcion) {
+        case 1: {
+            string nombres, apellidos, NIT, telefono, correo, fecha;
+            bool genero;
+            cout << "Nombres: "; getline(cin, nombres);
+            cout << "Apellidos: "; getline(cin, apellidos);
+            cout << "NIT: "; getline(cin, NIT);
+            cout << "Genero (1 = M, 0 = F): "; cin >> genero; cin.ignore();
+            cout << "Telefono: "; getline(cin, telefono);
+            cout << "Correo: "; getline(cin, correo);
+            cout << "Fecha_Ingreso: "; getline(cin, fecha);
+
+            c = Clientes(0, nombres, apellidos, NIT, genero, telefono, correo, fecha);
+            c.crear();
+            break;
+        }
+        case 2:
+            c.leer();
+            break;
+        case 3: {
+            int id;
+            string nombres, apellidos, NIT, telefono, correo, fecha;
+            bool genero;
+            cout << "ID del cliente a actualizar: "; cin >> id; cin.ignore();
+            cout << "Nombres: "; getline(cin, nombres);
+            cout << "Apellidos: "; getline(cin, apellidos);
+            cout << "NIT: "; getline(cin, NIT);
+            cout << "Genero (1 = M, 0 = F): "; cin >> genero; cin.ignore();
+            cout << "Telefono: "; getline(cin, telefono);
+            cout << "Correo: "; getline(cin, correo);
+            cout << "Fecha_Ingreso:"; getline(cin, fecha);
+
+            c.setIdCliente(id);
+            c.setNombres(nombres);
+            c.setApellidos(apellidos);
+            c.setNIT(NIT);
+            c.setGenero(genero);
+            c.setTelefono(telefono);
+            c.setCorreo(correo);
+            c.setFechaIngreso(fecha);
+            c.actualizar();
+            break;
+        }
+        case 4: {
+            int id;
+            cout << "ID del cliente a eliminar: "; cin >> id; cin.ignore();
+            c.setIdCliente(id);
+            c.borrar();
+            break;
+        }
+        case 5:
+            cout << "Saliendo del menu de clientes...\n";
+            break;
+        default:
+            cout << "Opcion invalida\n";
+        }
+    } while (opcion != 5);
+}
+
+
 int main() {
     int opcionPrincipal = 0;
     do {
         cout << "\n--- Menu Principal ---\n";
-        cout << "1. Empleados\n2. Clientes (pendiente)\n3. Productos (pendiente)\n4. Salir\n";
+        cout << "1. Empleados\n2. Clientes\n3. Productos (pendiente)\n4. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opcionPrincipal;
         cin.ignore();
@@ -101,7 +173,7 @@ int main() {
             menuEmpleados();
             break;
         case 2:
-            cout << "Modulo Clientes en desarrollo\n";
+            menuClientes();
             break;
         case 3:
             cout << "Modulo Productos en desarrollo\n";
